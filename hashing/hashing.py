034055c0934adede1649a2d774b2c109383b9823
Kip <kiptrujillo@protonmail.com>
2021-01-09 12:48:16 -0800

    updated comment for accuracy

--- hashing/hashing.py
+++ hashing/hashing.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 ##########################################
 # Date: January  8, 2020
-# Purpose:  Trying out the builtin hash() function. I learned that you cannot hash mutable data types like lists, sets or dictionaries. Object must be mutable like a Tuple
+# Purpose:  Trying out the builtin hash() function. I learned that you cannot hash mutable data types like lists, sets or dictionaries. Object must be immutable like a Tuple
 ##########################################
 count = int(input())
 x = str(input())
